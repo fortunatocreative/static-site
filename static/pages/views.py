a66f2374d3a573ccda3b2eadc07c872aba9411fa
Dave Vineis <dave.vineis@gmail.com>
2024-08-14 18:26:40 -0500

    move

--- static/pages/views.py
+++ static/pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-## {} are used to pass other python programs into page
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, "home.html", {})
 
 def about(request):
-    return render(request, 'about.html', {})
+    return render(request, "about.html", {})
+
+def contact(request):
+    return render(request, "contact.html", {})
